2f7e30ad0759e417ef9d4d4d14ebeca54fea4ab9
PremSagar <premsagarbandi.dev@gmail.com>
2026-09-05 20:35:18 +0530

    Implemented AppointmentDTOResponse for task 12

---
+++ src/main/java/com/flm/appointment/dto/response/AppointmentResponseDTO.java
@@ -0,0 +1,51 @@
+package com.flm.appointment.dto.response;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AppointmentResponseDTO {
+
+	private String appointmentId;
+	
+	private String patientId;
+	
+	private String patientName;
+	
+	private String doctorId;
+	
+	private String doctorName;
+	
+	private LocalDate appointmentDate;
+	
+	private LocalTime startTime;
+	
+	private LocalTime endTime;
+	
+	private String status;
+	
+	private String notes;
+	
+	private String reasonForVisit;
+	
+	private String diagnosisSummary;
+	
+	private String prescription;
+	
+	private String medicines;
+	
+	private String notesForReceptionist;
+	
+	private String followUpSuggestion;
+	
+	private String dietPlan;
+	
+}
